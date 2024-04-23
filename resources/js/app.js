9e9f5014634ee4b01f96c726bcd8fed5a45e027f
injammahin <injammahin5507@gmail.com>
2024-04-23 21:13:28 +0600

    sd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+// Import the necessary libraries and components
+import { createApp } from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import ExampleComponent from "./components/Todo.vue"; // Ensure this import is at the top for clarity
+
+// Create the Vue application
+const app = createApp({});
+
+// Use VueAxios plugin. This syntax is used for providing axios to the Vue application as a plugin
+app.use(VueAxios, axios);
+
+// Register your component globally
+app.component("todo", ExampleComponent);
+
+// Mount the application to the DOM element with the ID 'app'
+app.mount("#app");
---
+++ resources/js/components/Todo.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Todo</div>
+
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input
+                                type="text"
+                                placeholder="Todo.."
+                                class="form-control"
+                                aria-label="todo"
+                                aria-describedby="todo"
+                            />
+                            <div class="input-group-append">
+                                <button type="button" class="btn btn-info">
+                                    Add
+                                </button>
+                            </div>
+                            {{ todos }}
+                            <table class="table table-bordered mt-4">
+                                <thead>
+                                    <th>S.no</th>
+                                    <th>Name</th>
+                                    <th>Action</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos: [],
+            api: "http://localhost:8000/api/todos",
+        };
+    },
+    mounted() {
+        this.axios.get(this.api).then((res) => {
+            this.todos = res.data;
+        });
+    },
+};
+</script>
